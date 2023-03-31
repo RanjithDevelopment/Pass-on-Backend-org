4461f07675eee9e80774d84772440c5c334f31c8
RANJITH <73814077+RanjithDevelopment@users.noreply.github.com>
2023-03-31 12:44:38 +0530

    Update userRouter.js

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -4,4 +4,4 @@ const Auth = require("../Modules/Authmodule.js");
 const router=express.Router();
 router.get('/get',Auth.authorizeAdmin,usermodule.getUserData);
 router.delete('/delete/:id',Auth.authorizeAdmin,usermodule.deleteuserdata);
-module.exports=router;
+module.export=router;
