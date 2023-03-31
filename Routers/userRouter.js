99e7cbde816bfbd346718999b69a76150b40de9c
RANJITH <73814077+RanjithDevelopment@users.noreply.github.com>
2023-03-31 13:09:04 +0530

    Add files via upload

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -4,4 +4,4 @@ const Auth = require("../Modules/Authmodule.js");
 const router=express.Router();
 router.get('/get',Auth.authorizeAdmin,usermodule.getUserData);
 router.delete('/delete/:id',Auth.authorizeAdmin,usermodule.deleteuserdata);
-module.export=router;
+module.exports=router;
